c93de628da77cb7a83541ee3eac1f2e24507f86e
forrestmorrison <forrestmorrison@gmail.com>
2022-12-06 13:48:30 -0600

    added Mongo DB credentials, gitignore server index for now

--- server/index.js
+++ server/index.js
@@ -3,4 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://forrestmorrison:Techjunction11304@cluster0.ibtthaz.mongodb.net/?retryWrites=true&w=majority';
+
+// https://www.mongodb.com/cloud/atlas
