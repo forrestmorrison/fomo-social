1f3731755d157a4e6390c7b651f26cc182b38f46
forrestmorrison <forrestmorrison@gmail.com>
2022-12-06 10:47:52 -0600

    set up App & Index and deleted extra files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div>
+      <h1>App</h1>
     </div>
-  );
+  )
 }
 
 export default App;
